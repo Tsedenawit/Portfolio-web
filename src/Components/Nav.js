27ae54b19a5c0007c11be8a522952d5624313b3c
tsedi lemma <tsedenawit3@gmail.com>
2024-07-31 16:29:42 +0300

    first commit

---
+++ src/Components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Nav(){
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About me</li>
+                <li>Portfolio</li>
+                <li>Contact me</li>
+            </ul>
+        </div>
+    )
+}
