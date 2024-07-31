f2c541bc643da0623e8ed60a359ce441355339b1
tsedi lemma <tsedenawit3@gmail.com>
2024-07-31 16:49:06 +0300

    send

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Nav(){
     return(
-        <div>
-            <ul>
+        <div className=" h-[70px] bg-white drop-shadow-md flex justify-center items-center justify-betwwen">
+            <ul className="flex space-x-12">
                 <li>Home</li>
                 <li>About me</li>
                 <li>Portfolio</li>
