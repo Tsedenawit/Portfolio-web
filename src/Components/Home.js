fba03aed4e8767baae2d21a5d75703c1d7defa51
tsedi lemma <tsedenawit3@gmail.com>
2024-08-01 10:09:08 +0300

    sent

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import myImage from "../Images/Images.png"
 export default function Home(){
     return (
-        <div>
+        <div className="flex justify-center w-[1000px] items-center">
             <div >
             <h4 className="text-xs block">Hey, I am Tsedenawit Lemma</h4>
             <h1 className="text-6xl block block mb-4">I create <span className="text-purple-400 ">Projects</span></h1>
             <h1 className="text-6xl block block mb-4">and new experience</h1>
-            <h4 className="text-xs block block mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</h4>
+            <h4 className="text-xs block block mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in
+                eros elementum tristique.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum
+                tristique.</h4>
             </div>
             <div>
-                <img src="./Public/Images/images.png" alt="not found"></img>
+                <img src={myImage} alt="not found"></img>
             </div>
         </div>
     )
