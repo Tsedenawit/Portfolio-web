def0fdc3a0a8d1ecc58cf12d86478a9573fa3fdd
tsedi lemma <tsedenawit3@gmail.com>
2024-08-01 15:53:39 +0300

    updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,10 +2,10 @@ import React from "react";
 import Image from "../Images/about.png"
 export default function About(){
     return(
-        <div className="flex  justify-between">
-            <div><img src={Image} className="w-[550px]"></img></div>
-            <div className="w-[600px] mt-[100px] mr-[50px]">
-                <h4 className="text-4xl"><b>About Me</b> </h4>
+        <div className="flex justify-between ml-20 mt-[100px]">
+            <div><img src={Image} className="w-[650px]"></img></div>
+            <div className="w-[600px] mt-[250px] mr-[80px]">
+                <h4 className="text-4xl"><b>About Me</b> </h4><br></br>
                 <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus
                     massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu 
                     neque, tristique quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,12 +3,12 @@ import myImage from "../Images/Images.png"
 export default function Home(){
     return (
         <div id="cont" className="max-w-full overflow-x-hidden mx-auto">
-             <div className="flex justify-between items-center w-full overflow-x-hidden mx-auto lg:px-20 ml-20" >
+             <div className="flex justify-between items-center w-full overflow-x-hidden mx-auto lg:px-20 " >
             <div className="lg:w-[600px] w-full">
             <h4 className="text-xs block">Hey, I am Tsedenawit Lemma</h4>
-            <h1 className="text-6xl block block mb-4">I create <span className="text-violet-600 ">Projects</span></h1>
-            <h1 className="text-6xl block block mb-4">and new experience</h1>
-            <h4 className="text-xs block block mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in
+            <h1 className="text-6xl  block mb-4">I create <span className="text-violet-600 ">Projects</span></h1>
+            <h1 className="text-6xl  block mb-4">and new experience</h1>
+            <h4 className="text-xs  block mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in
                 eros elementum tristique.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum
                 tristique.</h4>
             <button className="text-center bg-violet-600 w-[150px] h-[50px] text-white rounded"> Get in touch</button>
