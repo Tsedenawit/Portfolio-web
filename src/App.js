25328ce8f5b2dda5f369e5f02e09ec7131bcaa20
tsedi lemma <tsedenawit3@gmail.com>
2024-08-01 10:24:49 +0300

    send

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Nav from './Components/Nav';
 import Home from './Components/Home';
+import About from './Components/About';
 function App() {
   return (
     <div >
      <Nav/><br></br>
      <Home/>
+     <br></br>
+     <About/>
     </div>
   );
 }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import Image from "../Images/about.png"
 export default function About(){
     return(
         <div>
-            <div><img src=""></img></div>
+            <div><img src={Image}></img></div>
         </div>
     )
 }
