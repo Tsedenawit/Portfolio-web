9b06f7cdae73193f20c789e43d2212e9d279e1f1
tsedi lemma <tsedenawit3@gmail.com>
2024-08-01 10:23:06 +0300

    update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,17 +2,18 @@ import React from "react";
 import myImage from "../Images/Images.png"
 export default function Home(){
     return (
-        <div className="flex justify-center w-[1000px] items-center">
-            <div >
+        <div className="flex justify-between items-center ml-20 mr-20">
+            <div className="w-[600px]">
             <h4 className="text-xs block">Hey, I am Tsedenawit Lemma</h4>
-            <h1 className="text-6xl block block mb-4">I create <span className="text-purple-400 ">Projects</span></h1>
+            <h1 className="text-6xl block block mb-4">I create <span className="text-violet-600 ">Projects</span></h1>
             <h1 className="text-6xl block block mb-4">and new experience</h1>
             <h4 className="text-xs block block mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in
                 eros elementum tristique.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum
                 tristique.</h4>
+            <button className="text-center bg-violet-600 w-[150px] h-[50px] text-white rounded"> Get in touch</button>
             </div>
             <div>
-                <img src={myImage} alt="not found"></img>
+                <img src={myImage} alt="not found" className="w-[600px]"></img>
             </div>
         </div>
     )
