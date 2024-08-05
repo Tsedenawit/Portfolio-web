d040d80a670a731b57ad624c5d5d71adb25dcbce
tsedi lemma <tsedenawit3@gmail.com>
2024-08-05 12:26:56 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import Port from './Components/Port';
 import Contact from './Components/Contact';
 function App() {
   const names =[
-  "Country quiz",
-  "Coffee page",
+  {name:"Country quiz"},
+  {Name:"Coffee page"},
   "Portfolio"
 
 ]
+const elements = names.map(na=>{
+  return <Port name={na.Name}/>
+})
   return (
     <div className='max-w-full h-auto'>
      <Nav/>
      <Home/>
      <About/>
      <div className="flex justify-between">
-     <Port name="Country Quiz" /> 
+     {elements}
      </div>
      <Contact/>
     </div>
