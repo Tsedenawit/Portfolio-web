4b821f6481c21c2a7c9cea6b02707c8ee5a7c14b
tsedi lemma <tsedenawit3@gmail.com>
2024-08-05 13:41:33 +0300

    updated

--- src/Components/Port.jsx
+++ src/Components/Port.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import React from 'react';
+import { Button, Modal } from 'antd';
 import images2 from "../Images/Image.png";
 import images1 from "../Images/quiz.png";
+import { useState } from 'react';
 
 export default function Port(props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -25,35 +23,33 @@ export default function Port(props) {
             src={props.image}
             className="max-w-100 max-h-50 min-h-[50px] rounded-md"
           ></img>
-            <div className="mt-3 pl-3">
-            <Button onClick={handleOpen}>{props.name}</Button>
+          <>
+            <Button type="dashed" onClick={showModal}>
+              Open Modal
+            </Button>
             <Modal
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
+              footer={null}
+              className="mt-[10rem]"
+              title="Basic Modal"
+              open={isModalOpen}
+              onOk={handleOk}
+              onCancel={handleCancel}
             >
-              <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Country quiz
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Duis mollis, est non commodo luctus, nisi erat porttitor
-                  ligula.
-                </Typography>
-              </Box>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
+              <p>Some contents...</p>
             </Modal>
-          </div>
+          </>
           <h2 className="mt-2 pl-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros.
           </h2>
           <button className="underline-offset-8 underline mt-4 pl-5">
-           <a href={props.links} target="_blank">View in Github</a> 
+            <a href={props.links} target="_blank">
+              View in Github
+            </a>
           </button>
         </div>
-       
-        
       </div>
     </div>
   );
