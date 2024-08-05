17c54be78802bfe684a23642432392dd79d9da53
tsedi lemma <tsedenawit3@gmail.com>
2024-08-05 13:52:33 +0300

    send

--- src/Components/Port.jsx
+++ src/Components/Port.jsx
@@ -18,30 +18,33 @@ export default function Port(props) {
   return (
     <div className="mb-[50px] ">
       <div className="flex justify-between mr-20 ml-20">
-        <div className="max-w-[450px] min-h-[450px] rounded-md mt-20 shadow-2xl ">
+        <div className="max-w-[450px] min-h-[500px] rounded-md mt-20 shadow-2xl ">
           <img
             src={props.image}
             className="max-w-100 max-h-50 min-h-[50px] rounded-md"
           ></img>
           <>
-            <Button type="dashed" onClick={showModal}>
-              Open Modal
+            <Button type="link" onClick={showModal}>
+              {props.name}
             </Button>
             <Modal
               footer={null}
-              className="mt-[10rem]"
-              title="Basic Modal"
+              centered={true}
+              title={props.name}
               open={isModalOpen}
               onOk={handleOk}
               onCancel={handleCancel}
             >
-              <p>Some contents...</p>
-              <p>Some contents...</p>
-              <p>Some contents...</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              varius enim in eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              varius enim in eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              varius enim in eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              varius enim in eros.</p>
             </Modal>
           </>
           <h2 className="mt-2 pl-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros.
           </h2>
           <button className="underline-offset-8 underline mt-4 pl-5">
