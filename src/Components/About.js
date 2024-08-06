cc91d8d9f4e877ed5779ed3be9e1b83a4ae7b2ac
tsedi lemma <tsedenawit3@gmail.com>
2024-08-06 14:32:05 +0300

    send

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,17 +3,15 @@ import Image from "../Images/about.png"
 export default function About(){
     return(
         <div id="about" className="flex justify-between ml-20 mt-[100px]">
-            <div><img src={Image} className="w-[500px]"></img></div>
-            <div className="w-[500px] mt-[200px] mr-[120px]">
+            <div><img src={Image} className="w-[700px] "></img></div>
+            <div className="w-[700px] mt-[300px] mr-[100px] pl-10">
                 <h4 className="text-4xl"><b>About Me</b> </h4><br></br>
                 <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius faucibus
                     massa sollicitudin amet augue. Nibh metus a semper purus mauris duis. Lorem eu 
                     neque, tristique quis duis. Nibh scelerisque ac adipiscing velit non nulla in amet 
-                    pellentesque.<br></br>
+                    pellentesque.<br></br><br></br>
                     Sit turpis pretium eget maecenas. Vestibulum dolor mattis consectetur eget commodo 
-                    vitae. Amet pellentesque sit pulvinar lorem mi a, euismod risus r.Sit turpis pretium 
-                    eget maecenas. Vestibulum dolor mattis consectetur eget commodo vitae. Amet pellentesque 
-                    sit pulvinar lorem mi a, euismod risus r.</h6>
+                    vitae. Amet pellentesque sit pulvinar lorem mi a, euismod risus r.</h6>
             </div>
         </div>
     )
