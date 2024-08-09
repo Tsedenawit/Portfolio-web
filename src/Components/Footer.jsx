4e6f7449dfbdfde32cf0541058c3b5d992260ba5
tsedi lemma <tsedenawit3@gmail.com>
2024-08-09 15:45:34 +0300

    sent

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,13 +9,13 @@ export default function Footer() {
    ] 
     return (
       <div id="footer" className="mt-20 mb-10">
-        <div className="w-full min-h-[150px] flex justify-center items-center">
-        <ul className="flex space-x-10">
+        <div className="w-full min-h-[150px] flex items-center">
+        <ul className="flex space-x-10 justify-center flex-grow">
         {Pages.map((data)=>(
           <li key={data.name}><a href={data.nav}>{data.name}</a></li>
         ))}
         </ul>
-        <div className="flex float-right justify-end space-x-2 items-end">
+        <div className="flex space-x-2 ml-auto mr-5">
         <FaFacebookF />
         <FaInstagram />
         <FaTwitter />
@@ -23,12 +23,12 @@ export default function Footer() {
         </div>
         </div>
         <div>
-        <hr className="border-t-2 border-slate-300 " />
+        <hr className="border-t-2 border-slate-300 ml-10 mr-10" />
         </div>
         <br></br>
-        <div className="flex justify-between min-h-[80px]">
+        <div className="flex justify-between min-h-[80px] ml-5 mt-5">
         <h3>Made by Tsedenawit Lemma</h3>
-        <div className="flex space-x-3">
+        <div className="flex space-x-3 mr-5">
         <h3>Privacy policy</h3>
         <h3>Terms of service</h3>
         <h3>Cookies setting</h3>  
